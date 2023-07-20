e3fd4af306aa35af6283321a12d153451a823e53
hoangvbm2808 <vbmhoang.2808@gmail.com>
2023-07-20 17:42:46 +0700

    old ver

--- app/src/main/java/com/example/farmmarket/FormLogin.java
+++ app/src/main/java/com/example/farmmarket/FormLogin.java
@@ -57,7 +57,7 @@ public class FormLogin extends AppCompatActivity {
 
                 RequestQueue queue = Volley.newRequestQueue(FormLogin.this);
 
-                String url = "http://10.17.37.245:9000/user/login";
+                String url = "http://192.168.1.7:9000/user/login";
 
                 HashMap<String,String> params = new HashMap<String, String>();
                 params.put("username",name);
